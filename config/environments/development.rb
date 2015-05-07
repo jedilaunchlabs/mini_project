e0c3d2e49390963889e03e198783b198857bf45d
Jedi Ballono <jedi@launchlabs.ph>
2015-05-07 18:27:20 +0800

    Initialize mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,4 +38,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address                => "smtp.gmail.com",
+    :port                   => 587,
+    :domain                 => "gmail.com",
+    :user_name              => "techstars.developers@gmail.com",
+    :password               => "th3r3dp1ll",
+    :authentication         => "plain",
+  }
 end
