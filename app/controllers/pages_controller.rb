f4d1f8850819b8d3fc9ce31e20e8f76734bf8f9c
Jedi Ballono <jedi@launchlabs.ph>
2015-05-11 17:54:35 +0800

    Display blogs by creation date and validate is_draft

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
 
 	def home
-		@blogs = Blog.paginate(:page => params[:page], :per_page => 5)
+		@blogs = Blog.order("created_at desc").paginate(:page => params[:page], :per_page => 5)
+    @blogs = @blogs.where(:is_draft => false)
 	end
 
 end
