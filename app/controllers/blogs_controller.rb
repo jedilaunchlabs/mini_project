eb809c636f10b46031bcbe050a2d0af7ec2851d8
Jedi Ballono <jedi@launchlabs.ph>
2015-05-14 14:31:10 +0800

    Add BlogImageString class and add process for fog configuration.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,14 @@
+class BlogImageString < StringIO
+  attr_accessor :original_filename, :content_type
+end 
+
 class BlogsController < ApplicationController
 
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  require 'base64'
+
+  before_action :set_blog, only: [:show, :edit, :update]
   before_filter :authenticate_user!, except: [:show]
+
 
   def index
     @blogs = current_user.blogs.order("created_at desc").paginate(:page => params[:page], :per_page => 5)
@@ -26,14 +33,25 @@ class BlogsController < ApplicationController
 
 
   def create
+    image = params[:image]
+
     @blog = current_user.blogs.build(blog_params)
 
     respond_to do |format|
       if @blog.save
 
+        if image.present?
+          image64 = image.split(",").second
+          io = BlogImageString.new(Base64.decode64(image64))
+          io.original_filename = "foobar.png"
+          io.content_type = "image/png"
+          @blog.image = io
+          @blog.save
+        end
+
         if params[:categories].present?
           params[:categories].each do |category|
-            @blogs_category = BlogsCategory.create({ blog_id: @blog.id, category_id: category.to_i })
+            @blog_category = BlogsCategory.create({ blog_id: @blog.id, category_id: category.to_i })
           end
         end
 
@@ -52,20 +70,20 @@ class BlogsController < ApplicationController
     respond_to do |format| 
       if @blog.update_attributes(blog_params) 
         if params[:categories].present?
-          @blogs_category = BlogsCategory.select("blog_id").where("blog_id = ? AND category_id NOT IN (?)", @blog.id, params[:categories])
+          @blog_category = BlogsCategory.select("blog_id").where("blog_id = ? AND category_id NOT IN (?)", @blog.id, params[:categories])
 
-          if @blogs_category.present?
-            @blogs_category.delete_all
+          if @blog_category.present?
+            @blog_category.delete_all
           end
 
           params[:categories].each do |category|
-            @blogs_category = BlogsCategory.select("blog_id").where("blog_id = ? AND category_id IN (?)", @blog.id, category.to_i)
-            unless @blogs_category.present?
-              @blogs_category = BlogsCategory.create({ blog_id: @blog.id, category_id: category.to_i })
+            @blog_category = BlogsCategory.select("blog_id").where("blog_id = ? AND category_id IN (?)", @blog.id, category.to_i)
+            unless @blog_category.present?
+              @blog_category = BlogsCategory.create({ blog_id: @blog.id, category_id: category.to_i })
             end
           end
         else
-          @blogs_category = BlogsCategory.select("blog_id").where("blog_id = ?", @blog.id).delete_all
+          @blog_category = BlogsCategory.select("blog_id").where("blog_id = ?", @blog.id).delete_all
         end
         
         format.html { redirect_to blogs_path, notice: 'Blog was successfully updated!' }
@@ -86,7 +104,7 @@ class BlogsController < ApplicationController
       end
 
     def blog_params
-        params.require(:blog).permit(:title, :caption, :description, :image)
+        params.require(:blog).permit(:title, :caption, :description)
       end
 
 end
